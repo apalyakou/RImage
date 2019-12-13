98d67a2b1cc1cd6257204792fe575b878c995ba8
Anton Paliakov <toxa95401@gmail.com>
2019-12-14 02:57:41 +0300

    Add RImage library.

---
+++ RImage/RImage/RImage.h
@@ -0,0 +1,19 @@
+//
+//  RImage.h
+//  RImage
+//
+//  Created by Anton Polyakov on 12/14/19.
+//  Copyright © 2019 by.anton.paliakou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RImage.
+FOUNDATION_EXPORT double RImageVersionNumber;
+
+//! Project version string for RImage.
+FOUNDATION_EXPORT const unsigned char RImageVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RImage/PublicHeader.h>
+
+

5d3c65985a0934c77a1977c0012ce09b6d6bf747
Anton Paliakov <toxa95401@gmail.com>
2019-12-14 14:03:12 +0300

    Add basic RImage & RImageViewModel.

---
+++ RImage/RImage/RImageViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  RImageViewModel.swift
+//  RImage
+//
+//  Created by Anton Polyakov on 12/14/19.
+//  Copyright © 2019 by.anton.paliakou. All rights reserved.
+//
+
+import Foundation
+
+class RImageViewModel: ObservableObject {
+
+}

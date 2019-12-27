185de3d3dbfee895bed135b045d0b606e8c2b2d8
Anton Paliakov <toxa95401@gmail.com>
2019-12-27 19:07:56 +0300

    Update NetworkServiceTests.

--- RImage/RImageTests/NetworkServiceTests.swift
+++ RImage/RImageTests/NetworkServiceTests.swift
@@ -7,17 +7,26 @@
 //
 
 import XCTest
-import UIKit
+@testable import RImage
 
 class NetworkServiceTests: XCTestCase {
 
-    func testLoadImage() {
+    func testLoadStubImage() {
         let url = URL(string: "stub/url")!
         let imageDataStub = UIImage(systemName: "remote_image")?.jpegData(compressionQuality: 1)
         let networkService = NetworkServiceStub()
         
         networkService.load(url: url, completion: { data in
             XCTAssertEqual(data, imageDataStub)
+        })
+    }
+    
+    func testLoadRemoteImage() {
+        let url = URL(string: "https://developer.apple.com/assets/elements/icons/swiftui/swiftui-128x128_2x.png")!
+        let networkService = NetworkService()
+        
+        networkService.load(url: url, completion: { data in
+            XCTAssertNotNil(data)
         })
     }
 }
